d8123b88b6f843c5543c8f634205bc0a57a0588b
Giboney <70110379+Giboney@users.noreply.github.com>
2025-02-23 20:33:14 -0800

    dummy set?

--- src/js/data/sets/gen10.js
+++ src/js/data/sets/gen10.js
@@ -1,11 +1,3 @@
 var SETDEX_PATHWAYS = {
-	"Abomasnow": {
-		"Abomasnow": {
-			"ability": "Snow Warning",
-			"evs": {"hp": 248, "df": 130, "sd": 132},
-			"item": "Assault Vest",
-			"nature": "Sassy",
-			"moves": ["Blizzard", "Wood Hammer"]
-		}
-	}
+	"Abomasnow": {}
 };
